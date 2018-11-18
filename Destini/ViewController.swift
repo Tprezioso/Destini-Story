1a6e78b6f9971e36ce736d649fc9ff01c2116c0d
Tprezioso <tommyprezioso@gmail.com>
2018-11-18 12:55:34 -0500

    finished choose your own adventure story app

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // Our strings
+    // Strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
     let answer1a = "I\'ll hop in. Thanks for the help!"
     let answer1b = "Better ask him if he\'s a murderer first."
@@ -29,37 +29,74 @@ class ViewController: UIViewController {
     let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
     
     
-    // UI Elements linked to the storyboard
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
     
-    // TODO Step 5: Initialise instance variables here
-    
-    
-    
+    var storyIndex : Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
+        restart()
     }
 
+    @IBAction func restartPressed(_ sender: UIButton) {
+        restart()
+    }
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
+            switch (sender.tag, storyIndex) {
+            case (1,1) :
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+                storyIndex = 3
+            case (1,3) :
+                storyTextView.text = story6
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+                storyIndex = 6
+            case (2,1) :
+                storyTextView.text = story2
+                topButton.setTitle(answer2a, for: .normal)
+                bottomButton.setTitle(answer2b, for: .normal)
+                storyIndex = 2
+            case (2,3) :
+                storyTextView.text = story5
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+                storyIndex = 5
+            case (1,2) :
+                storyTextView.text = story3
+                topButton.setTitle(answer3a, for: .normal)
+                bottomButton.setTitle(answer3b, for: .normal)
+                storyIndex = 3
+            case (2,2) :
+                storyTextView.text = story4
+                topButton.isHidden = true
+                bottomButton.isHidden = true
+                storyIndex = 4
+            default:
+                print("done")
+            }
         
-    
+        if storyIndex == 4 || storyIndex == 5 || storyIndex == 6 {
+            restartButton.isHidden = false
+        }
     }
     
-
-
+    func restart() {
+        storyIndex = 1
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        restartButton.isHidden = true
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+    }
 
 }
 
